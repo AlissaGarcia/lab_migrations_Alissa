310b83d373765aa9c216c039e09e1ff636f280f5
Alissa Garcia <118315759+AlissaGarcia@users.noreply.github.com>
2026-06-17 03:07:06 +0000

    feat: cria migrations completas do sistema de gestao de projetos

---
+++ lab_migrations/database/migrations/2026_06_17_030340_create_projeto_colaborador_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projeto_colaborador', function (Blueprint $table) {
+    $table->id();
+    $table->foreignId('projeto_id')
+          ->constrained('projetos')
+          ->onDelete('cascade');
+    $table->foreignId('colaborador_id')
+          ->constrained('colaboradores')
+          ->onDelete('cascade');
+    $table->string('funcao');
+    $table->date('data_entrada');
+    $table->timestamps();
+    $table->unique(['projeto_id', 'colaborador_id']); // unicidade composta
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projeto_colaborador');
+    }
+};
